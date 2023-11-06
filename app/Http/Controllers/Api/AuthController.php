6fbc5cff9dee78b840e55b4717fcc16d7e3c16bc
Andrii <andrii.yefros@gmail.com>
2023-11-06 13:09:42 +0200

    feat: adding sanctum auth login

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * Login The User
+     *
+     * @var Request
+     */
+    public function login(Request $request): JsonResponse
+    {
+        try {
+            $validateUser = Validator::make($request->all(), [
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validation Error',
+                    'errors' => $validateUser->errors(),
+                ], 401);
+            }
+
+            if (! Auth::attempt($request->only(['email', 'password']))) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & Password does not match with our record',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            // Revoke all tokens...
+            $user->tokens()->delete();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'token' => $user->createToken('MY API TOKEN')->plainTextToken,
+            ]);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+}
