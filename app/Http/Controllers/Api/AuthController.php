b585d8bfa33a6ffcf100457fbe939454de846938
Andrii <andrii.yefros@gmail.com>
2023-11-28 05:56:47 +0200

    feat: adding user form request

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,36 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     /**
      * Login The User
      *
-     * @param \Illuminate\Http\Request $request
+     * @param UserRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function login(Request $request): JsonResponse
+    public function login(UserRequest $request): JsonResponse
     {
         try {
-            $validateUser = Validator::make($request->all(), [
-                'email' => 'required|email',
-                'password' => 'required',
-            ]);
-
-            if ($validateUser->fails()) {
-                return response()->json([
-                    'success' => false,
-                    'message' => 'Validation error',
-                    'errors' => $validateUser->errors(),
-                ], 401);
-            }
-
             if (! Auth::attempt($request->only(['email', 'password']))) {
                 return response()->json([
                     'success' => false,
